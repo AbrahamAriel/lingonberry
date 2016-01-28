4782a26474b9c5de4f3bfd58c8ff264ecdda2b2f
acrox999 <acrox999@gmail.com>
2016-01-28 19:03:42 +0800

    Added favicon support

--- php/head.php
+++ php/head.php
@@ -32,5 +32,8 @@
 <!-- Custom Fonts -->
 <link href="//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic|Raleway:600,500,400" rel="stylesheet" type="text/css">
 
+<!-- Favicon -->
+<link rel="shortcut icon" href="<?php echo $Site->url() ?>favicon.png" type="image/x-icon">
+
 <!-- Plugins in Site head -->
 <?php Theme::plugins('siteHead') ?>
